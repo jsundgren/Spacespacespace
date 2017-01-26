ba2d1b958fcaed8f9a4245cff31084be1cf9e6f1
elinlager <elinlager@c-152-25.eduroam.liu.se>
2017-01-26 15:02:12 +0100

    matlab osv

---
+++ MATLAB/planet.m
@@ -0,0 +1,14 @@
+classdef planet
+
+    
+    properties
+        mass            %Mass of planet (kg)
+        position        %Position of the planet, vector of three [x, y, z]
+        velocity        %Velocity of planet, vector of three [x, y, z]
+    end
+    
+    methods
+    end
+    
+end
+
