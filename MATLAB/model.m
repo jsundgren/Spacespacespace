f93fcbf296bc5dc3986e096dfbd1e9ace1ed6323
elinlager <elinlager@c-152-96.eduroam.liu.se>
2017-01-27 14:04:53 +0100

    pål plott

--- MATLAB/model.m
+++ MATLAB/model.m
@@ -9,18 +9,33 @@
 % test 1
 
 %clear, clc
+%u=10^24;
+%planet1 = planet(3*u, [0 0 0], [0 0 0], [0 0 0]);
+%planet2 = planet(4*u, [0 0 0], [1 2 0], [0 0 0]);
+%planet3 = planet(5*u, [0 0 0], [2 0 0], [0 0 0]);
+%planet4 = planet(6*u, [0 0 0], [2 0 1], [0 0 0]);
+%system = [planet1 planet2 planet3 planet4];
 
-planet1 = planet(3, [0 0 0], [1 1 0], [0 0 0]);
-planet2 = planet(4, [0 0 0], [1 2 0], [0 0 0]);
-planet3 = planet(5, [0 0 0], [2 0 0], [0 0 0]);
-planet4 = planet(6, [0 0 0], [2 0 1], [0 0 0]);
 
-system = [planet1 planet2 planet3 planet4];
+mercury = planet()
 
 newSystem = sumForceSystem( system );
 
-for i = 1:length(system)
-    scatter3(system(i).position(1), system(i).position(2), system(i).position(3));
-    hold on
+time=10;
+
+
+for j=1:time
+    for i = 1:length(system)
+        scatter3(system(i).position(1), system(i).position(2), system(i).position(3));
+        system(i).position
+        hold on
+        %axis([-1 3 -1 3 -1 3]);
+
+    end
+    system = sumForceSystem(system);
+    system = nextPosition(system);
+    hold off
+    pause;
 end
+
 
