119fe32de8d3ff1a8a3e9aab62d8ee4b09473871
Anton Pålsson <antonpaalsson@gmail.com>
2017-02-06 00:29:24 +0100

    Polski pushki

--- js/space.js
+++ js/space.js
@@ -1,106 +1,98 @@
+
+var renderer, scene, camera, light, cameraControls;
+var mouseX = 0, mouseY = 0;
+
+init();
+animate();
+
 function init() {
-	/****** VARIABLES *********/
 
+	// CREATE RENDERER
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-	/****** CREATE SCENE AND CAMERA *********/
-			var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-			camera.position.set(0, 0, -40);
+	// CREATE SCENE & CAMERA
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	camera.position.set(0, 0, -3);
 
+	// CREATE LIGHT SOURCES
+	light = new THREE.DirectionalLight( 0xffffff );
+	scene.add( light );
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+	// SET ORBIT CONTROLS
+	cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
 
-	/****** LIGHT SOURCES *********/
-			var flashlight = new THREE.PointLight(0xffffff, 1, 100);
-			flashlight.target = camera;
-			flashlight.position.copy(camera.position);
-			scene.add(flashlight);
+	cameraControls.enableDamping = true;
+	cameraControls.dampingFactor = 0.25;
+	cameraControls.enableZoom = true;
+	cameraControls.minDistance = 10;
+	cameraControls.maxDistance = 60;
 
-			scene.add(new THREE.AmbientLight(0x404040));
+	// EVENT LISTENERS
+	window.addEventListener( 'resize', onWindowResize, false );
 
+	addModel(0, 0, 0);
+}
 
-			/*var spotLight       = new THREE.SpotLight( 0xffffff );
-			 spotLight.position.set( 10, 10, 10 );
-			 scene.add( spotLight );
+// RINGS
+function addRing(radiusIn, radiusOut, planet, colorRing) {
 
-			 var light           = new THREE.AmbientLight( 0x404040 );
-			 scene.add( light );*/
+	var geometryRing = new THREE.RingGeometry( radiusIn, radiusOut, 50 );
+	var materialRing = new THREE.MeshBasicMaterial( { color: colorRing, transparent: true, opacity: 0.6, side: THREE.DoubleSide} );
+	materialRing.side = THREE.DoubleSide;
+	var rings = new THREE.Mesh( geometryRing, materialRing );
+	rings.rotateX(3.14/2.5);
+	scene.add(rings);
+}
 
+function addModel( x, y, z ) {
 
-    /****** SUN *********/
-			var geometry = new THREE.IcosahedronGeometry(5, 1)
+	var r = THREE.Math.randFloat( 1.5, 3 );
+	var m = r/5;
 
-			var material = new THREE.MeshPhongMaterial( {color: Math.random()*0xffffff} );
-            material.shading = THREE.FlatShading;
+	var geometry = new THREE.IcosahedronGeometry( r, 1 );
+	var material = new THREE.MeshPhongMaterial( { color: Math.random() * 0xffffff } );
+	material.shading = THREE.FlatShading;
 
-			var sphere = new THREE.Mesh( geometry, material );
-			scene.add( sphere );
+	var model = new THREE.Mesh( geometry, material );
+	scene.add( model );
 
-            console.log(sphere);
-
-            
-            for(i=0; i < sphere.geometry.vertices.length; i = i+3) {
-                sphere.geometry.vertices[i].x = sphere.geometry.vertices[i].x + Math.floor((Math.random() * 10) + 1)/10;
-                sphere.geometry.vertices[i].y = sphere.geometry.vertices[i].y + Math.floor((Math.random() * 10) + 1)/20;
-                sphere.geometry.vertices[i].z = sphere.geometry.vertices[i].z + Math.floor((Math.random() * 10) + 1)/15;
-            }
-            sphere.VerticesNeedUpdate = true;
-
-
-	/******* RINGS *********/
-
-            function addRing(radiusIn, radiusOut, planet, colorRing) {
-			var geometryRing = new THREE.RingGeometry( radiusIn, radiusOut, 50 );
-			var materialRing = new THREE.MeshBasicMaterial( { color: colorRing, transparent: true, opacity: 0.6, side: THREE.DoubleSide} );
-			materialRing.side = THREE.DoubleSide;
-			var rings = new THREE.Mesh( geometryRing, materialRing );
-			rings.rotateX(3.14/2.5);
-			scene.add(rings);
-			}
-
-			addRing(6, 6.8, sphere, Math.random()*0xffffff);
-			addRing(7.5, 8.7, sphere, Math.random()*0xffffff);
-
-
-
-    /****** ORBIT CONTROLS *********/
-			var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-			controls.enableDamping = true;
-			controls.dampingFactor = 0.25;
-			controls.enableZoom = true;
-
-			controls.minDistance = 10;
-			controls.maxDistance = 60;
-
-
-
-	//updatera så att ljuset följer kameran
-			controls.addEventListener('change', light_update)
-
-			function light_update() {
-				flashlight.position.copy(camera.position);
-				}
-
-			function onWindowResize() {
-					camera.aspect = window.innerWidth / window.innerHeight;
-					camera.updateProjectionMatrix();
-					renderer.setSize( window.innerWidth, window.innerHeight );
-				}
-
-	/****** RENDER FUNCTION *********/
-			var render = function () {
-				requestAnimationFrame( render );
-				controls.update();
-
-				sphere.rotation.x += 0.0049;
-				sphere.rotation.y += 0.005;
-
-				renderer.render(scene, camera);
-			};
-
-			window.addEventListener( 'resize', onWindowResize, false );
-			render();
+	for( i = 0; i < model.geometry.vertices.length; i = i + 3 ) {
+		model.geometry.vertices[i].x += THREE.Math.randFloatSpread( m );
+		model.geometry.vertices[i].y += THREE.Math.randFloatSpread( m );
+		model.geometry.vertices[i].z += THREE.Math.randFloatSpread( m );
 	}
+
+	model.VerticesNeedUpdate = true;
+
+	model.position.x = x;
+	model.position.y = y;
+	model.position.z = z;
+
+	if( THREE.Math.randInt( 1, 3 ) == 3 ) {
+
+		var inner = r + THREE.Math.randFloat( 0.8, 1.2 );
+		var outer = inner + THREE.Math.randFloat( 1, 1.3 );
+
+		addRing( inner, outer, model, Math.random()*0xffffff);
+	}
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate () {
+
+	requestAnimationFrame( animate );
+	cameraControls.update();
+	light.position.set( camera.position.x, camera.position.y, camera.position.z ).normalize();
+
+	renderer.render( scene, camera );
+};
