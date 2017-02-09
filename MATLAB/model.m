fd4b6ff279b2967829443f0d524a9b012a9f8139
ElonOlsson <olssonelon@gmail.com>
2017-02-09 12:02:58 +0100

    nothing

--- MATLAB/model.m
+++ MATLAB/model.m
@@ -10,15 +10,15 @@ v = 0.0005;
 u = 10^9;
 sun = planet(333000, [0 0 0], [0 0 0], [0 0 0]);
 terra = planet(1, [0 v 0], [do 0 0], [0 0 0]);
-lunar = planet(1/6, [0 v 0], [do+0.257 0 0], [0 0 0]);
-%lunar = planet(1.3, [-v 0 0], [0 do 0], [0 0 0]);
-%smoon = planet(1.3, [0 -v 0], [-do 0 0], [0 0 0]);
-%kerbal = planet(1.3, [v 0 0], [0 -do 0], [0 0 0]);
+%lunar = planet(1/6, [0 v 0], [do+0.257 0 0], [0 0 0]);
+lunar = planet(1.3, [-v 0 0], [0 do 0], [0 0 0]);
+smoon = planet(1.3, [0 -v 0], [-do 0 0], [0 0 0]);
+kerbal = planet(1.3, [v 0 0], [0 -do 0], [0 0 0]);
 
 
-system = [terra sun lunar];
+system = [terra sun lunar smoon kerbal];
 
-time = 880;
+time = 80;
 steplength = 8000;
 
 
